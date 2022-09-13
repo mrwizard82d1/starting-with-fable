d81916a54ae8a110a4ec4d5cabfe8ef335bf6bf4
laj <larry.jones@reveal-energy.com>
2022-09-13 10:39:08 -0500

    Demonstrate calling a third-party JavaScript package
    This commit is so cool! Drawing a spinning 3-D box on a canvas element.

--- src/App.fs
+++ src/App.fs
@@ -2,36 +2,31 @@ module App
 
 open Fable.Core
 
-// Define interfaces to the DOM. These interfaces are actually available from `Fable.Browser.Dom`. We do it
-// manually for "experience". (This experience allows us to see and do what needs to be done to interact with
-// native JavaScript.)
+// Manually created p5.js interface
+// See [Starting with Fable](https://itnext.io/starting-with-fable-f-83846ab790ad) for alternatives for
+// generating the interop code.
 
-// A DOM node
-type Node =
-    abstract appendChild: child: Node -> Node
-    abstract insertBefore: node: Node * ?child: Node -> Node
+[<StringEnum>]
+type Renderer =
+    | [<CompiledName("p2d")>] P2D
+    | [<CompiledName("webgl")>] WebGL
     
-// The browser document
-type Document =
-    abstract createElement: tagName: string -> Node
-    abstract createTextNode: text: string -> Node
-    abstract getElementById: elementId: string -> Node
-    abstract body: Node with get, set
+type [<Import("*", "p5/lib/p5.js")>] p5(?sketch: p5 -> unit, ?id: string) =
+    member __.setup with set(v: unit -> unit): unit = jsNative
+    member __.draw with set(v: unit -> unit): unit = jsNative
+    member __.createCanvas(w: float, h: float, ?renderer: Renderer): unit = jsNative
+    member __.background(value: int): unit = jsNative
+    member __.millis(): float = jsNative
+    member __.rotateX(angle: float): unit = jsNative
+    member __.box(): unit = jsNative
     
-// Define `document` as the native JavaScript `document`
-let [<Global>] document: Document = jsNative
-
 // Client code
+let sketch (it: p5) =
+    it.setup <- fun() -> it.createCanvas(300., 300., WebGL)
+    it.draw <- fun () ->
+        it.background(255)
+        it.rotateX(it.millis() / 1000.)
+        it.box()
 
-// Create a new `div` Node attached to the document (but not in the node tree)
-let newDiv = document.createElement("div")
-
-// Insert a text node into the newly created `div`
-"Good news everyone! Generated dynamically by Fable!"
-|> document.createTextNode
-|> newDiv.appendChild
-|> ignore
-
-// Insert the newly created node (and its child text node) into the node tree
-let currentDiv = document.getElementById("app")
-document.body.insertBefore (newDiv, currentDiv) |> ignore
+// draw
+p5(sketch) |> ignore
