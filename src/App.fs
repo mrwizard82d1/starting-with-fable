b03bcfc39c75191b7760cbfaa88eae278584c487
laj <larry.jones@reveal-energy.com>
2022-09-12 20:13:23 -0500

    Demonstrate JS interop using interfaces

--- src/App.fs
+++ src/App.fs
@@ -2,4 +2,17 @@ module App
 
 open Fable.Core
 
-JS.console.log "Hello from Fable!"
+// The "interface" approach
+
+// Interface used to "define" the JavaScript global `window` object.
+type Window =
+    // We are only interested in the `alert` function. The return type and argument type are "defined" by the
+    // JavaScript documentation of the `alert` function.
+    abstract alert: ?message: string -> unit
+    
+// Wire-up JavaScript and F# with `[<Global>]` attribute and `jsNative`.
+let [<Global>] window: Window = jsNative
+
+// Actual client calls
+window.alert ("Global Fable window.alert")  // with parentheses
+window.alert "Global Fable window.alert sans parentheses"
